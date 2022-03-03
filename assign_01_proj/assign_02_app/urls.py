eb5665e4ea265ddc1982df17abf7c2d21f5d9674
hoangdigan <42528214+hoangdigan@users.noreply.github.com>
2022-03-03 13:58:45 +0700

    Assign_02 _03 Mar 2022

---
+++ assign_01_proj/assign_02_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = "assign_02_app"
+urlpatterns = [
+   # Assign 2 - Excercise 1 - Create 2 views
+   path('ex1/', views.index, name='index'),
+   path('ex1/<int:person_id>/', views.detail, name='detail'),
+
+   # Assign 2 - Excercise 2 - Create 2 view - use templates
+   # Generic View
+   path('ex2/', views.IndexView.as_view(), name='index'),
+   path('ex2/<int:pk>/', views.DetailView.as_view(), name='detail'),
+]
---
+++ assign_01_proj/assign_02_app/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from assign_01_app.models import Person
+
+def index(request):
+    response = HttpResponse()
+    person_list = Person.objects.order_by('id')[:10]
+    for p in person_list:
+        output = ', '.join([str(p.id), p.name, str(p.age), p.address, p.mobile_number])
+        response.write(output)
+        response.write("<br>")
+    return HttpResponse(response)
+
+def detail(request, person_id):
+    response = HttpResponse()
+    p = Person.objects.get(id=person_id)
+    output = '<br>'.join([str(p.id), p.name, str(p.age), p.address, p.mobile_number])
+    response.write(output)
+    return HttpResponse(response)
+
+#  Exercise 2 : use templates
+
+class IndexView(generic.ListView):
+    template_name = 'assign_02_app/index.html'
+    context_object_name = 'person_list'
+
+    def get_queryset(self):
+        return Person.objects.all
+
+
+class DetailView(generic.DetailView):
+    model = Person
+    template_name = 'assign_02_app/detail.html'
+
+
