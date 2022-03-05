5dd1ea5ee9f15047b9c68a4a940017d0d1bc92fa
hoangdigan <42528214+hoangdigan@users.noreply.github.com>
2022-03-05 10:06:55 +0700

    Assign_05 _03 Mar 2022 L2

--- assign_01_proj/assign_02_app/views.py
+++ assign_01_proj/assign_02_app/views.py
@@ -46,18 +46,3 @@ def formInput(request):
     context['form'] = form
     return render(request, "assign_02_app/form.html", context)
 
-
-def formInput(request):
-    context = {}
-
-    # create object of form
-    form = PersonForm(request.POST or None, request.FILES or None)
-
-    # check if form data is valid
-    if form.is_valid():
-        # save the form data to model
-        form.save()
-
-    context['form'] = form
-    return render(request, "assign_02_app/form.html", context)
-
